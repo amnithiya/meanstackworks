a412d7828b0fcd046536c40b436292f10d65556b
Nithiya Thomas <thomasnithiya@gmail.com>
2021-09-09 11:29:59 +0530

    msg

---
+++ javascriptworks/script.js
@@ -0,0 +1,36 @@
+//  javascript programming
+// print()  =>python
+// printf() =>c
+// cout<<   =>c++
+// system.out.println() =>java
+// console.log("") =>javascript
+
+//--------- identifiers------------------
+
+// simply name is called identifier
+// int age=17 =>variable name lower_case,noun,underscore
+
+// function displayValues() =>displayValues =>functionname
+// print_person() snake notation
+// printPerson() camel case ->javascript
+
+// class person{} =>person =>classname PersonDetails()
+
+
+// variables
+// to represent a memory location
+
+// int num=2;
+// memory store data    num=10234|0010
+
+
+
+
+var company="luminar technolab";
+var loc_name="kakkanad"
+console.log(company);
+console.log(loc_name);
+
+
+//age name
+// hai i am nam...age years old
